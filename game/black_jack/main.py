ec4538cc04c83b4df19e6244d4dc7ee49c841a84
hhwenyu <hhwenyu@gmail.com>
2022-01-19 00:40:47 -0800

    update black jack game

--- game/black_jack/main.py
+++ game/black_jack/main.py
@@ -20,6 +20,7 @@ class Player(Deck):
 	
 	cards_value = {1 :1 , 2: 2 , 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11:10, 12:10, 13:10}
 	def __init__(self,name, card=[], value = 0):
+		super().__init__()
 		self.name = name
 		self.card = card
 		self.value = value
@@ -28,22 +29,30 @@ class Player(Deck):
 		self.card.append(card)
 		self.value += Player.cards_value[card] 
 		
-		
+	def winning_prob(self, cards):
+		self.cards = cards
+		#print(len(self.cards),self.cards)
+		#for unseen_card in self.cards:
+		#	print(unseen_card, int(unseen_card % 13),Deck.cards_point[int(unseen_card % 13)] )
+		return sum(Deck.cards_point[int(unseen_card % 13)] < ( 21 - self.value ) for unseen_card in self.cards) / len(self.cards)
+
+
 cards = Deck()
 
 run = 0
 p1 = Player('dealer',[])
 p2 = Player('player',[])
-
+#print(p1.winning_prob(cards.cards))
+#print(p2.winning_prob(cards.cards))
 while run <= 10:
 	run +=1
 	p1.add_card(cards.pick_a_card()[1]) 
 	print(f"Run:{run}")
 	print(f"{p1.name} got {p1.card} with total value {p1.value}; the prob of winning: {float(sum(Deck().cards_point[int(card % 13)] < 21-p1.value for card in cards.cards)/len(cards.cards))}")
-	print(f"the prob of winning: num: {sum( Deck().cards_point[int(card % 13)] < 21-p1.value for card in cards.cards)}; and denum: {len(cards.cards)}")
+	print(f"the prob of winning: {p1.winning_prob(cards.cards)}")
 	p2.add_card(cards.pick_a_card()[1]) 
 	print(f"{p2.name} got {p2.card} with total value {p2.value}")
-	print(f"the prob of winning: num: {sum( Deck().cards_point[int(card % 13)] < 21-p2.value for card in cards.cards)}; and denum: {len(cards.cards)}")
+	print(f"the prob of winning: {p2.winning_prob(cards.cards)}")
         
 	if p1.value > 21 or p2.value >21:
 		print("Game Over!")
